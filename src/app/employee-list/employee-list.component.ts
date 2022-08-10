c71e4392901f01bf941525cb52e25fb6b6fbe50c
itsmeeAbhi <abhishekp1912@gmail.com>
2022-08-10 17:03:56 +0530

    initial commit

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employee : any = [];
+  constructor(private empservice: EmployeeService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.empservice.getEmplist().subscribe(
+      data =>{
+        this.employee = data,
+        console.log("Response Received")
+      },
+      error =>console.log("Error Occured")
+    )
+  }
+  addEmployee(){
+    this.router.navigate(['add-employee']);
+  }
+
+}
